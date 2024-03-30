bbee16b8201d7565d4067451ae19ab5e64310ad6
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-03-30 11:00:48 -0300

    .

--- src/br/com/alura/screenmatch/modelos/Episodio.java
+++ src/br/com/alura/screenmatch/modelos/Episodio.java
@@ -1,2 +1,41 @@
-package br.com.alura.screenmatch.modelos;public class Episodio {
+package br.com.alura.screenmatch.modelos;
+import br.com.alura.screenmatch.calculos.Classificacao;
+
+public class Episodio implements Classificacao {
+
+    private int numero;
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    private String nome;
+
+    public void setSerie(Serie serie) {
+        this.serie = serie;
+    }
+
+    private Serie serie;
+    private int totalVisualizacoes;
+
+    public int getTotalVisualizacoes() {
+        return totalVisualizacoes;
+    }
+
+    public void setTotalVisualizacoes(int totalVisualizacoes) {
+        this.totalVisualizacoes = totalVisualizacoes;
+    }
+    @Override
+    public int getClassificacao() {
+        if (totalVisualizacoes > 100) {
+            return 4;
+        } else {
+            return 2;
+        }
+    }
 }
